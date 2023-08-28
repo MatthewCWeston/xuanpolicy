290aa2233ecec4ebe2c8ac1fe583a62df3d4fd21
MatthewCWeston <61944935+MatthewCWeston@users.noreply.github.com>
2023-08-28 03:51:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         "gym==0.26.2",
         "gymnasium==0.28.1",
         "gym-notices==0.0.8",
-        "box2d-py==2.3.5",
         "mpi4py==3.1.3",
         "tqdm==4.62.3",
         "pyglet==1.5.15",
